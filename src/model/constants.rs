a9f1939048fa3880a5e1586447cc6bb9505bf301
Chiffa <artemy040404@gmail.com>
2024-02-10 23:36:46 +0300

    Replace manual default RatingConstants implementation with a trait implementation

--- src/model/constants.rs
+++ src/model/constants.rs
@@ -11,20 +11,22 @@ pub struct RatingConstants {
     pub volatility_growth_rate: f64
 }
 
-pub fn default_constants() -> RatingConstants {
-    let multiplier = 45;
-    let sigma = (25.0 / 5.0) * multiplier as f64;
+impl Default for RatingConstants {
+    fn default() -> Self {
+        let multiplier = 45;
+        let sigma = (25.0 / 5.0) * multiplier as f64;
 
-    RatingConstants {
-        multiplier,
-        default_mu: 15.0 * multiplier as f64,
-        default_sigma: sigma,
-        default_tau: sigma / 100.0,
-        default_beta: sigma / 2.0,
-        default_kappa: 0.0001,
-        decay_minimum: 825.0,
-        decay_days: 115,
-        decay_rate: 0.06 * multiplier as f64,
-        volatility_growth_rate: 0.08 * (i32::pow(multiplier, 2) as f64),
+        Self {
+            multiplier,
+            default_mu: 15.0 * multiplier as f64,
+            default_sigma: sigma,
+            default_tau: sigma / 100.0,
+            default_beta: sigma / 2.0,
+            default_kappa: 0.0001,
+            decay_minimum: 825.0,
+            decay_days: 115,
+            decay_rate: 0.06 * multiplier as f64,
+            volatility_growth_rate: 0.08 * (i32::pow(multiplier, 2) as f64),
+        }
     }
 }
