80b8b071d49393b7f264a0260785314816fe4775
hburn7 <hburnett777@gmail.com>
2024-02-16 18:20:06 -0500

    Reference MULTIPLIER when creating DECAY_MINIMUM

--- src/model/constants.rs
+++ src/model/constants.rs
@@ -6,7 +6,7 @@ pub static TAU: f64 = SIGMA / 100.0;
 pub static BETA: f64 = SIGMA / 2.0;
 pub static KAPPA: f64 = 0.0001;
 pub static DECAY_DAYS: u64 = 115;
-pub static DECAY_MINIMUM: f64 = 825.0;
+pub static DECAY_MINIMUM: f64 = MULTIPLIER * 18.0;
 pub static DECAY_RATE: f64 = 0.06 * MULTIPLIER;
 
 lazy_static! {
