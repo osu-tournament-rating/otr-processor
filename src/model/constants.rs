93d277f831f71a44f07531531583baf4c92e29a5
hburn7 <38370573+hburn7@users.noreply.github.com>
2024-02-20 18:15:58 -0500

    Apply suggestions from code review
    Co-authored-by: cytusine0 <139372927+cytusine0@users.noreply.github.com>

--- src/model/constants.rs
+++ src/model/constants.rs
@@ -1,12 +1,12 @@
 // Model constants
 pub static MULTIPLIER: f64 = 45.0;
-pub static SIGMA: f64 = (25.0 / 5.0) * MULTIPLIER;
+pub static SIGMA: f64 = 5.0 * MULTIPLIER;
 pub static MU: f64 = 15.0 * MULTIPLIER;
 pub static TAU: f64 = SIGMA / 100.0;
 pub static BETA: f64 = SIGMA / 2.0;
 pub static KAPPA: f64 = 0.0001;
 pub static DECAY_DAYS: u64 = 115;
-pub static DECAY_MINIMUM: f64 = MULTIPLIER * 18.0;
+pub static DECAY_MINIMUM: f64 = 18.0 * MULTIPLIER;
 pub static DECAY_RATE: f64 = 0.06 * MULTIPLIER;
 // Default rating constants for osu!
 pub static OSU_RATING_FLOOR: f64 = 5.0;
