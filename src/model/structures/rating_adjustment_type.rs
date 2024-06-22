66bc622d677757758eb82295ae09f6cac779350c
hburn7 <hburnett777@gmail.com>
2024-06-21 22:21:15 -0400

    adjustment type

---
+++ src/model/structures/rating_adjustment_type.rs
@@ -0,0 +1,20 @@
+use serde_repr::{Deserialize_repr, Serialize_repr};
+use std::convert::TryFrom;
+
+#[derive(Deserialize_repr, Serialize_repr, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum RatingAdjustmentType {
+    Decay = 0,
+    Match = 1
+}
+
+impl TryFrom<i32> for RatingAdjustmentType {
+    type Error = ();
+    fn try_from(v: i32) -> Result<Self, Self::Error> {
+        match v {
+            0 => Ok(RatingAdjustmentType::Decay),
+            1 => Ok(RatingAdjustmentType::Match),
+            _ => Err(())
+        }
+    }
+}
