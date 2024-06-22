8e377267c7384f10c2c4cb821a293744fcd9822d
hburn7 <hburnett777@gmail.com>
2024-06-22 19:46:41 -0400

    cleanup mod

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -2,13 +2,4 @@ mod constants;
 mod decay;
 mod rating_tracker;
 pub mod structures;
-
-use openskill::{
-    constant::{DEFAULT_BETA, KAPPA},
-    model::plackett_luce::PlackettLuce,
-    rating::default_gamma
-};
-
-pub fn model() -> PlackettLuce {
-    PlackettLuce::new(DEFAULT_BETA, KAPPA, default_gamma)
-}
+pub mod model;
