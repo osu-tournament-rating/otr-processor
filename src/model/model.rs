f8046a7aed1cbd801d8ded5861a3f43ef364e64e
hburn7 <hburnett777@gmail.com>
2024-06-22 20:43:36 -0400

    add game rating with test

--- src/model/model.rs
+++ src/model/model.rs
@@ -1,4 +1,9 @@
-use openskill::{constant::*, model::plackett_luce::PlackettLuce, rating::default_gamma};
+use openskill::{
+    constant::*,
+    model::{model::Model, plackett_luce::PlackettLuce},
+    rating::{default_gamma, Rating}
+};
+use std::collections::HashMap;
 
 use crate::{
     api::api_structs::{Game, Match, PlayerRating},
@@ -44,12 +49,47 @@ impl OtrModel {
     /// 5. Update the RatingTracker after the match is processed.
     fn process_match(&self, m: &Match) {}
 
-    fn rate(&self, g: &Game, ruleset: Ruleset) {
-        let tuple: (Vec<Option<&PlayerRating>>, Vec<i32>) = g
+    /// Rates a Game. Returns a HashMap of <player_id, (new_rating, new_volatility)
+    fn rate(&self, game: &Game, ruleset: Ruleset) -> HashMap<i32, (f64, f64)> {
+        let (ratings, placements): (Vec<Option<&PlayerRating>>, Vec<usize>) = game
             .placements
             .iter()
-            .map(|p| (self.rating_tracker.get_rating(p.player_id, ruleset), p.placement))
+            .map(|p| {
+                (
+                    self.rating_tracker.get_rating(p.player_id, ruleset),
+                    p.placement as usize
+                )
+            })
             .collect();
+
+        let mut teams = Vec::new();
+        for r in &ratings {
+            match r {
+                Some(p_rating) => {
+                    teams.push(vec![Rating {
+                        mu: p_rating.rating,
+                        sigma: p_rating.volatility
+                    }]);
+                }
+                None => panic!("Expected player to have a rating!")
+            }
+        }
+
+        if teams.len() != placements.len() {
+            panic!("Expected rating and placement lengths to be identical!")
+        }
+
+        let results = self.model.rate(teams, placements);
+        let mut changes = HashMap::new();
+
+        for i in 0..ratings.len() {
+            let p_rating = ratings.get(i).unwrap();
+            let result = results.get(i).unwrap().get(0).unwrap();
+
+            changes.insert(p_rating.unwrap().player_id, (result.mu, result.sigma));
+        }
+
+        changes
     }
 
     /// Applies a scaled performance penalty to negative changes in rating.
@@ -67,5 +107,96 @@ impl OtrModel {
         // Rating differential is used with a scaling factor
         // to determine final rating change
         rating.rating = scaling * (rating_diff * (games_played as f64 / games_total as f64))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::api::api_structs::{Game, PlayerPlacement, PlayerRating};
+    use crate::model::model::OtrModel;
+    use crate::model::structures::rating_adjustment_type::RatingSource;
+    use crate::model::structures::ruleset::Ruleset;
+
+    #[test]
+    fn test_rate() {
+        let mut model = OtrModel::new();
+
+        // Add 3 players to model
+        model.rating_tracker.insert_or_update(
+            &PlayerRating {
+                player_id: 1,
+                ruleset: Ruleset::Osu,
+                rating: 1000.0,
+                volatility: 100.0,
+                percentile: 0.0,
+                global_rank: 0,
+                country_rank: 0,
+                timestamp: Default::default(),
+                source: RatingSource::Match,
+                adjustments: Vec::new()
+            },
+            &"US".to_string()
+        );
+        model.rating_tracker.insert_or_update(
+            &PlayerRating {
+                player_id: 2,
+                ruleset: Ruleset::Osu,
+                rating: 1000.0,
+                volatility: 100.0,
+                percentile: 0.0,
+                global_rank: 0,
+                country_rank: 0,
+                timestamp: Default::default(),
+                source: RatingSource::Match,
+                adjustments: Vec::new()
+            },
+            &"US".to_string()
+        );
+        model.rating_tracker.insert_or_update(
+            &PlayerRating {
+                player_id: 3,
+                ruleset: Ruleset::Osu,
+                rating: 1000.0,
+                volatility: 100.0,
+                percentile: 0.0,
+                global_rank: 0,
+                country_rank: 0,
+                timestamp: Default::default(),
+                source: RatingSource::Match,
+                adjustments: Vec::new()
+            },
+            &"US".to_string()
+        );
+
+        let game = Game {
+            id: 0,
+            game_id: 0,
+            start_time: Default::default(),
+            end_time: None,
+            placements: vec![
+                PlayerPlacement {
+                    player_id: 1,
+                    placement: 2
+                },
+                PlayerPlacement {
+                    player_id: 2,
+                    placement: 1
+                },
+                PlayerPlacement {
+                    player_id: 3,
+                    placement: 3
+                },
+            ]
+        };
+
+        let rating_result = model.rate(&game, Ruleset::Osu);
+
+        // Compare the 3 rating values, ensure order is 2, 1, 3
+        let result_1 = rating_result.get(&1).unwrap().0;
+        let result_2 = rating_result.get(&2).unwrap().0;
+        let result_3 = rating_result.get(&3).unwrap().0;
+
+        assert!(result_2 > result_1);
+        assert!(result_1 > result_3);
     }
 }
