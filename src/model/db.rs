ac3e9997b7cca8d909613f97e065afbdb7a3f31f
hburn7 <hburnett777@gmail.com>
2024-09-03 22:48:47 -0400

    main query implemented

--- src/model/db.rs
+++ src/model/db.rs
@@ -1,4 +1,4 @@
-use crate::model::db_structs::{NewMatch, NewTournament};
+use crate::model::db_structs::{NewGame, NewGameScore, NewMatch, NewTournament};
 use crate::model::structures::ruleset::Ruleset;
 use std::sync::Arc;
 use tokio_postgres::{Client, Error, NoTls};
@@ -36,50 +36,16 @@ impl DbClient {
         FROM tournaments t
         LEFT JOIN matches m ON t.id = m.tournament_id
         LEFT JOIN games g ON m.id = g.match_id
-        LEFT JOIN game_scores gs ON g.id = gs.game_id
-        WHERE t.verification_status = 2 AND m.verification_status = 2", &[]).await.unwrap();
+        LEFT JOIN game_scores gs ON g.id = gs.game_id", &[]).await.unwrap();
 
         // TODO: Add 'WHERE t.processing_status = 4' to the query
         // TODO: Change 'WHERE t.verification_status = 2' to 'WHERE t.verification_status = 4'
         // TODO: Change 'WHERE m.verification_status = 2' to 'WHERE m.verification_status = 4'
-        for row in rows {
-            let tournament_id: i32 = row.get("tournament_id");
-            let match_id: i32 = row.get("match_id");
-            let game_id: i32 = row.get("game_id");
-            let game_score_id: i32 = row.get("game_score_id");
-
-            let mut tournament = NewTournament {
-                id: tournament_id,
-                name: row.get("tournament_name"),
-                ruleset: Ruleset::try_from(row.get::<&str, i32>("tournament_ruleset")).unwrap(),
-                matches: vec![],
-            };
-
-            // Check if the match already exists
-            let match_pos = tournament.matches.iter_mut().position(|m| m.id == match_id);
-
-            let match_ = if let Some(pos) = match_pos {
-                // If the match exists, return a mutable reference to it
-                &mut tournament.matches[pos]
-            } else {
-                // If the match doesn't exist, push a new match and then return a mutable reference to it
-                tournament.matches.push(NewMatch {
-                    id: match_id,
-                    name: row.get("match_name"),
-                    start_time: row.get("match_start_time"),
-                    end_time: row.get("match_end_time"),
-                    ruleset: tournament.ruleset,
-                    games: Vec::new(),
-                });
-                tournament.matches.last_mut().unwrap()
-            };
-
-            println!("Tournament: {:?}", tournament);
-            tournaments.push(tournament);
-        }
-
+        
         tournaments
     }
+
+    
 
     // Access the underlying Client
     pub fn client(&self) -> Arc<Client> {
